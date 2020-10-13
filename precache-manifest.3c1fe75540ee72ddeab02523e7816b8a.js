830dcdd2679de45cea0db1f7fa5fb9feacebf8f7
mengzhao <280810593@qq.com>
2020-10-12 21:14:30 -0400

    Updates

--- precache-manifest.3c1fe75540ee72ddeab02523e7816b8a.js
+++ precache-manifest.3c1fe75540ee72ddeab02523e7816b8a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a6a5d5ad0ed350d3690527c33f8683f5",
+    "revision": "a7d7afac9aca95ca6e0ff9a1668cce05",
     "url": "/swallowblack/robofriends/index.html"
   },
   {
-    "revision": "abcf402c9619777cd68e",
+    "revision": "939c29a12718c22b5acf",
     "url": "/swallowblack/robofriends/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "7e5e1754745ca199da5a",
+    "revision": "7fea859989d7c5025695",
     "url": "/swallowblack/robofriends/static/css/main.a47e8ab0.chunk.css"
   },
   {
-    "revision": "abcf402c9619777cd68e",
-    "url": "/swallowblack/robofriends/static/js/2.cab51068.chunk.js"
+    "revision": "939c29a12718c22b5acf",
+    "url": "/swallowblack/robofriends/static/js/2.e7e24cc0.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/swallowblack/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+    "url": "/swallowblack/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7e5e1754745ca199da5a",
-    "url": "/swallowblack/robofriends/static/js/main.bc16fd05.chunk.js"
+    "revision": "7fea859989d7c5025695",
+    "url": "/swallowblack/robofriends/static/js/main.e73d0d1d.chunk.js"
   },
   {
     "revision": "36d2b796c3cb8f424147",
